723b5a223ba89e08a18aeade3bec2bca94594aa4
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-04 19:28:39 +0200

    Configure SignalR Client Js

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,24 @@
+﻿// Create connection
+const userCountConnection =
+    new signalR.HubConnectionBuilder().withUrl('/hubs/userCount').build();
+
+// listen to hub methods invoke
+userCountConnection.on('updateTotalViews', (totalViews) => {
+    const totalViewsCounterSpan = document.getElementById('totalViewsCounter');
+    totalViewsCounterSpan.innerText = totalViews.toString();
+})
+
+//  invoke hub methods
+const newWindowLoadedOnClient = () => {
+    userCountConnection.send('NewWindowLoaded');
+}
+
+// Start Connection
+userCountConnection
+    .start()
+    .then(() => {
+        console.log("---Connection succeeded---");
+        newWindowLoadedOnClient();
+    }).catch(() => {
+    console.log("---Connection failed---")
+})
