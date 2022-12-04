de0429d66d21a28cbb585500059851dba38e4846
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-04 09:45:59 +0200

    Add methods to a Hub

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -4,5 +4,13 @@ namespace SignalRSample.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; }
+
+        public async Task NewWindowLoaded()
+        {
+            // send total count to all clients
+            TotalViews++;
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
     }
 }
